8b083329300da11ea9297580ce11837f31625cee
ipurnell <ianpurnell01@gmail.com>
2024-03-06 20:53:20 -0700

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 import './App.css';
+import schoolNames from './CollegeBasketballTeams.json';
+
+interface SchoolProps {
+  school: string;
+  name: string;
+  city: string;
+  state: string;
+}
+
+function SiteIntro() {
+  return <h1 className="my-5">March Madness</h1>;
+}
+
+class School extends React.Component<SchoolProps> {
+  render() {
+    const oneSchool = this.props;
+
+    return (
+      <div className="col-md-4 mb-4">
+        <div className="card">
+          <div className="card-body">
+            <h2 className="card-title">{oneSchool.school}</h2>
+            <h3 className="card-subtitle mb-2 text-muted">
+              Mascot: {oneSchool.name}
+            </h3>
+            <p className="card-text">
+              Location: {oneSchool.city}, {oneSchool.state}
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+function SchoolList() {
+  return (
+    <div className="row">
+      {schoolNames.teams.map((schoolNum: SchoolProps) => (
+        <School {...schoolNum} key={schoolNum.school} />
+      ))}
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SiteIntro />
+      <SchoolList />
     </div>
   );
 }
